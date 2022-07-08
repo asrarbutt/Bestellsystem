e6b0b8903c08254761a21ca3d754a4f944c94893
asrarbutt <asrarbutt1986@gmail.com>
2022-07-08 13:42:13 +0200

    add all attributes

--- src/main/java/db/ProductRepo.java
+++ src/main/java/db/ProductRepo.java
@@ -1,4 +1,31 @@
 package db;
 
+import model.Product;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ProductRepo {
+
+    Map<String, Product> products=new HashMap<>();
+
+
+    public ProductRepo(Map<String, Product> products) {
+        this.products = products;
+    }
+
+    public Map<String, Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Map<String, Product> products) {
+        this.products = products;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductRepo{" +
+                "products=" + products +
+                '}';
+    }
 }
--- src/main/java/model/Order.java
+++ src/main/java/model/Order.java
@@ -1,4 +1,45 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Order {
+    private String orderId;
+    List<Product> productList=new ArrayList<>();
+
+
+
+    public Order() {
+    }
+
+    public Order(String orderId, List<Product> productList) {
+        this.orderId = orderId;
+        this.productList = productList;
+    }
+
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(List<Product> productList) {
+        this.productList = productList;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId='" + orderId + '\'' +
+                ", productList=" + productList +
+                '}';
+    }
 }
